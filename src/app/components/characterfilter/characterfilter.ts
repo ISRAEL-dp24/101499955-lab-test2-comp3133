e8a9eccd5fd385121619c99902d81f7b35c6a648
ISRAEL_dp24 <dolapoosunkoya5@gmail.com>
2026-04-08 15:13:18 -0400

    add characterfilter component

--- src/app/components/characterfilter/characterfilter.ts
+++ src/app/components/characterfilter/characterfilter.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { HpService } from '../../services/hp.service';
+import { Character } from '../../models/character.model';
 
 @Component({
   selector: 'app-characterfilter',
+  standalone: true,
   imports: [],
   templateUrl: './characterfilter.html',
-  styleUrl: './characterfilter.css',
+  styleUrl: './characterfilter.css'
 })
-export class Characterfilter {}
+export class CharacterfilterComponent {
+  houses = ['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw'];
+  characters: Character[] = [];
+
+  constructor(private hpService: HpService) {}
+
+  onHouseChange(event: Event) {
+    const house = (event.target as HTMLSelectElement).value;
+    if (house) {
+      this.hpService.getCharactersByHouse(house).subscribe(data => {
+        this.characters = data;
+      });
+    } else {
+      this.characters = [];
+    }
+  }
+}

751c0af5fb49bfbf5880eabaa6ecfc8eef641ec5
ISRAEL_dp24 <dolapoosunkoya5@gmail.com>
2026-04-08 15:12:01 -0400

    add characterlist component

--- src/app/components/characterlist/characterlist.ts
+++ src/app/components/characterlist/characterlist.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HpService } from '../../services/hp.service';
+import { Character } from '../../models/character.model';
 
 @Component({
   selector: 'app-characterlist',
+  standalone: true,
   imports: [],
   templateUrl: './characterlist.html',
-  styleUrl: './characterlist.css',
+  styleUrl: './characterlist.css'
 })
-export class Characterlist {}
+export class CharacterlistComponent implements OnInit {
+  characters: Character[] = [];
+
+  constructor(private hpService: HpService, private router: Router) {}
+
+  ngOnInit() {
+    this.hpService.getAllCharacters().subscribe(data => {
+      this.characters = data;
+    });
+  }
+
+  goToDetails(id: string) {
+    this.router.navigate(['/character', id]);
+  }
+}

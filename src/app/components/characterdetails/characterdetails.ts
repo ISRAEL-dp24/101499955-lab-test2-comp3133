60a854bff226c8754a2d97bfcde6b690ae977e64
ISRAEL_dp24 <dolapoosunkoya5@gmail.com>
2026-04-08 15:14:39 -0400

    add characterdetails component

--- src/app/components/characterdetails/characterdetails.ts
+++ src/app/components/characterdetails/characterdetails.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HpService } from '../../services/hp.service';
+import { Character } from '../../models/character.model';
 
 @Component({
   selector: 'app-characterdetails',
+  standalone: true,
   imports: [],
   templateUrl: './characterdetails.html',
-  styleUrl: './characterdetails.css',
+  styleUrl: './characterdetails.css'
 })
-export class Characterdetails {}
+export class CharacterdetailsComponent implements OnInit {
+  character: Character | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private hpService: HpService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.hpService.getCharacterById(id).subscribe(data => {
+        this.character = data[0];
+      });
+    }
+  }
+
+  goBack() {
+    this.router.navigate(['/']);
+  }
+}
